bd4c7fdc11e3bcca470006a0e1cdd64720de101c
Bernardo Ardini <bernardo.ardini@gmail.com>
2026-06-14 00:30:05 +0200

    fatto

--- ephemeros/asmbvec.m
+++
@@ -1,8 +0,0 @@
-function out=asmbvec(int,k,j,i,space)
-    va=int.*reshape(space.geo.gauss{k}.trace{j}.measure,1,[]);
-    va=va(:);
-    ro=space.ind{i};
-    ro=ro(:);
-    mask=ro>0;
-    out=accumarray(ro(mask),va(mask),[space.ndof,1]);
-end
